27f9b39a88094f36012aed5b186662ebbd9804bf
lidiaapushkar <pushkar.sliceplanet@dmail.com>
2024-01-15 21:06:44 +0200

    homework1

---
+++ first/js/script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+let x = 84,
+    y = 17;
+
+const sum = x + y,
+      difference = x - y,
+      product = x * y,
+      quotient = x / y,
+      exponentiation = x**2;
+
+console.log(`Sum: ${sum}`);
+console.log(`Difference: ${difference}`);
+console.log(`Product: ${product}`);
+console.log(`Quotient: ${quotient}`);
+console.log(`Exponentiation: ${exponentiation}`);
