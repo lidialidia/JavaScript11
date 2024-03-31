188326b08e4dbb90bf58d245c9cf199d0c909e01
Lidiia Madelyk <l.madelyk@lavinmedia.com>
2024-03-31 19:45:53 +0300

    hw13 fixes

--- hw13/js/script.js
+++ hw13/js/script.js
@@ -1,6 +1,5 @@
 "use strict"
 
-const LOCAL_STORAGE_CURRENT_STATE = 'current_state';
 const LOCAL_STORAGE_LAST_STATE = 'last_state';
 const LOCAL_STORAGE_LAST_DATE = 'last_date';
 const TEXT_TURN_ON = 'turn on';
@@ -8,21 +7,20 @@ const TEXT_TURN_OFF = 'turn off';
 
 const switchButton = document.querySelector('.switch');
 
+const format = (element) => element.length < 2 ? `0${element}` : element
+
+const getCurrentState = (lastState) => lastState === TEXT_TURN_OFF ? TEXT_TURN_ON : TEXT_TURN_OFF
+
 // changes on click
 const getDateOfLastChange = () => {
     let currentDate = new Date(),
-        day = String(currentDate.getDate()),
-        month = String(currentDate.getMonth() + 1),
-        hours = String(currentDate.getHours()),
-        minutes = String(currentDate.getMinutes()),
-        seconds = String(currentDate.getSeconds());
+        day = format(String(currentDate.getDate())),
+        month = format(String(currentDate.getMonth() + 1)),
+        hours = format(String(currentDate.getHours())),
+        minutes = format(String(currentDate.getMinutes())),
+        seconds = format(String(currentDate.getSeconds()));
 
-    month = month.length < 2 ? `0${month}` : month;
-    day = day.length < 2 ? `0${day}` : day;
-    hours = hours.length < 2 ? `0${hours}` : hours;
-    minutes = minutes.length < 2 ? `0${minutes}` : minutes;
-    seconds = seconds.length < 2 ? `0${seconds}` : seconds;
-    
+
     return `${day}-${month}-${currentDate.getFullYear()} ${hours}:${minutes}:${seconds}`
 }
 
@@ -38,23 +36,22 @@ const appendParagraph = (text) => {
 const switchColorAndText = (event) => {
     let button =  event.target;
     const lastState = button.textContent.toLowerCase();
-    let currentState = lastState === TEXT_TURN_OFF ? TEXT_TURN_ON : TEXT_TURN_OFF;
 
-    button.textContent = currentState;
+    button.textContent = getCurrentState(lastState);
     appendParagraph(`Last ${lastState} at ${getDateOfLastChange()}`);
     document.querySelector('.wrapper').classList.toggle('dark');
 
-    localStorage.setItem(LOCAL_STORAGE_CURRENT_STATE, currentState);
     localStorage.setItem(LOCAL_STORAGE_LAST_STATE, lastState);
     localStorage.setItem(LOCAL_STORAGE_LAST_DATE, getDateOfLastChange());
 }
 
 const initTheme = () => {
-    const currentState = localStorage.getItem(LOCAL_STORAGE_CURRENT_STATE);
     const lastState = localStorage.getItem(LOCAL_STORAGE_LAST_STATE);
     const lastDate = localStorage.getItem(LOCAL_STORAGE_LAST_DATE);
     
-    if(currentState && lastState && lastDate) {
+    if(lastState && lastDate) {
+        const currentState = getCurrentState(lastState);
+
         appendParagraph(`Last ${lastState} at ${lastDate}`);
         switchButton.textContent = currentState;
 
