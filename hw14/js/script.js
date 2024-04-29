538d4adfb9359a8d4049eb2314456626c3849e74
Lidiia Madelyk <l.madelyk@lavinmedia.com>
2024-04-29 12:21:07 +0200

    hw14 add edit task, fix removeTaskFromStorage, add stripHtmlTags

--- hw14/js/script.js
+++ hw14/js/script.js
@@ -12,62 +12,98 @@ const filterInput = document.querySelector(".filter-input");
 
 // "storage" functions
 const getTasksFromStorage = () => {
-  return JSON.parse(localStorage.getItem(TASKS_STORAGE_KEY)) || [];
+    return JSON.parse(localStorage.getItem(TASKS_STORAGE_KEY)) || [];
 };
 
-const storeTaskInStorage = (newTask) => {
-  const tasks = getTasksFromStorage();
-  tasks.push(newTask);
+const storeTaskInStorage = (newTask, taskId) => {
+    const tasks = getTasksFromStorage();
+    tasks.push([newTask, taskId]);
 
-  localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(tasks));
+    localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(tasks));
 };
+
+const findElementInStorage = (tasks, key) => {
+    const foundArray = tasks.find(subarray => subarray.includes(key));
+    return foundArray;
+}
 
 const clearTasksFromStorage = () => {
   localStorage.removeItem(TASKS_STORAGE_KEY);
 };
 
-const removeTaskFromStorage = (deletedTask) => {
-  const tasks = getTasksFromStorage();
+const removeTaskFromStorage = (key) => {
+    const tasks = getTasksFromStorage();
 
-  const deletedIndex = tasks.findIndex((task) => task === deletedTask);
-  tasks.splice(deletedIndex, 1);
+    const deletedIndex = tasks.findIndex((task) => task[1] === key);
+    tasks.splice(deletedIndex, 1);
 
-  localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(tasks));
+    localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(tasks));
 };
 
-//
+const editTaskInStorage = (newTask, key) => {
+    const tasks = getTasksFromStorage();
+
+    tasks.forEach(task => {
+        if(task[1] === key) {
+            task[0] = newTask
+        }
+    })
+
+    localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(tasks));
+};
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+function stripHtmlTags(input) {
+    return input.replace(/<[^>]*>/g, '');
+}
 
 // "tasks" functions
-const appendLi = (value) => {
-  // Create and add LI element
-  const li = document.createElement("li");
-  // li.textContent = value; // Значення яке ввів користувач
-  li.innerHTML = `<span class="collection__text">${value}</span><span class="collection__btns"><i class="fa fa-remove delete-item"></i> <i class="fa fa-edit edit-item"></i></span>`;
-  taskList.append(li);
+const appendLi = (value, key) => {
+    // Create and add LI element
+    const li = document.createElement("li");
+    li.setAttribute('data-key', key);
+    // li.textContent = value; // Значення яке ввів користувач
+
+    li.innerHTML = `<span class="collection__text">${stripHtmlTags(value)}</span><span class="collection__btns"><i class="fa fa-remove delete-item"></i> <i class="fa fa-edit edit-item"></i></span>`;
+    taskList.append(li);
 };
 
 const addTask = (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  // Перевірка на пусте значення
-  const value = taskInput.value.trim();
-  if (value === "") {
-    return;
-  }
+    // Перевірка на пусте значення
+    const value = taskInput.value.trim();
+    if (value === "") {
+        return;
+    }
 
-  appendLi(value);
+    const key = makeid(5);
 
-  // Очистити форму
-  // 1 - скидає значення у input'a taskInput
-  taskInput.value = "";
-  // 2 - скидає всі значення форми
-  // form.reset();
+    appendLi(value, key);
+    // Зберігаємо елемент у localStorage
+    storeTaskInStorage(value, key);
 
-  // Фокусуємось на input
-  taskInput.focus();
+    // Очистити форму
+    // 1 - скидає значення у input'a taskInput
+    taskInput.value = "";
+    // 2 - скидає всі значення форми
+    // form.reset();
 
-  // Зберігаємо елемент у localStorage
-  storeTaskInStorage(value);
+    // Фокусуємось на input
+    taskInput.focus();
+
+ 
 };
 
 const clearTasks = () => {
@@ -76,22 +112,21 @@ const clearTasks = () => {
 };
 
 const removeTask = (event) => {
-  const isDeleteButton = event.target.classList.contains("delete-item");
-  if (!isDeleteButton) {
-    return;
-  }
-
-  const isConfirmed = confirm("Ви впевнені що хочете видалити це завдання?");
-  if (!isConfirmed) {
-    return;
-  }
-
-  const li = event.target.closest("li");
-  li.remove();
-
-  // Видалити зі сховища
-  const deletedTask = li.textContent.trim();
-  removeTaskFromStorage(deletedTask);
+    const isDeleteButton = event.target.classList.contains("delete-item");
+    if (!isDeleteButton) {
+      return;
+    }
+  
+    const isConfirmed = confirm("Ви впевнені що хочете видалити це завдання?");
+    if (!isConfirmed) {
+      return;
+    }
+  
+    const li = event.target.closest("li");
+    let liKey = li.getAttribute('data-key');
+    li.remove();
+  
+    removeTaskFromStorage(liKey);
 };
 
 const editTask = (event) => {
@@ -100,11 +135,15 @@ const editTask = (event) => {
         return;
     }
 
+    const li = event.target.closest("li");
+    let liKey = li.getAttribute('data-key');
     const newTask = prompt("Введіть нову назву задачі, будь ласка:");
 
-    event.target.parentElement.parentElement.querySelector('.collection__text').innerHTML = `${newTask}`;
-    
+
+    li.querySelector('.collection__text').innerHTML = `${newTask}`;
+    editTaskInStorage(newTask, liKey)
 }
+
 
 const filterTasks = ({ target: { value } }) => {
   const text = value.toLowerCase();
@@ -112,20 +151,15 @@ const filterTasks = ({ target: { value } }) => {
 
   list.forEach((li) => {
     const liText = li.textContent.trim().toLowerCase();
-
-    // if (liText.includes(text)) {
-    //   li.hidden = false;
-    // } else {
-    //   li.hidden = true;
-    // }
     li.hidden = !liText.includes(text);
   });
 };
 
 const initTasks = () => {
   const tasks = getTasksFromStorage();
-  // tasks.forEach((task) => appendLi(task));
-  tasks.forEach(appendLi);
+  tasks.forEach(task => {
+    appendLi(task[0], task[1]);
+  });
 };
 
 // Init
